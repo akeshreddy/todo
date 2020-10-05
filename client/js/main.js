9cd24b04ce61c9a456adc3a2a90376266b5206d2
akeshreddy <44496922+akeshreddy@users.noreply.github.com>
2020-10-05 17:12:02 +0530

    Add files via upload

---
+++ client/js/main.js
@@ -0,0 +1,33 @@
+
+Meteor.subscribe('todos');
+
+
+Template.hello.helpers({
+	'todos' : function(){
+		return Todos.find({userId: Meteor.userId()}, {sort :{ createdAt : -1 }});
+	},
+});
+
+Template.hello.events({
+	'submit .newTodo' : function(event){
+		event.preventDefault();
+		var text = event.target.text.value;
+		
+		Meteor.call('addTodo', text);
+		event.target.text.value= "";
+	},
+	'click .toogle-check': function(event){
+
+		Meteor.call('setChecked', this._id, !this.checked);
+	},
+	'click .delete-todo': function(){
+
+		Meteor.call('deleteTodo', this._id);
+		
+	},
+});
+
+Accounts.ui.config({
+	passwordSignupFields: 'USERNAME_ONLY'
+});
+
